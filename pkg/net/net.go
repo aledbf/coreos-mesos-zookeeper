ba2e0cf202fe9380f3d5d484e02fc3f61b9a39f1
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2015-05-26 00:35:19 -0300

    ref(*): remove boilerplate

--- pkg/net/net.go
+++ pkg/net/net.go
@@ -26,11 +26,12 @@ func WaitForPort(proto string, ip string, port int, timeout time.Duration) error
 }
 
 // RandomPort return a random not used TCP port
-func RandomPort() string {
-	l, _ := net.Listen("tcp", "127.0.0.1:0")
+func RandomPort(proto string) int {
+	l, _ := net.Listen(proto, "127.0.0.1:0")
 	defer l.Close()
 	port := l.Addr()
-	return strings.Split(port.String(), ":")[1]
+	lPort, _ := strconv.Atoi(strings.Split(port.String(), ":")[1])
+	return lPort
 }
 
 // GetNetworkInterfaces return the list of
--- pkg/net/net_test.go
+++ pkg/net/net_test.go
@@ -1,38 +1,19 @@
 package net
 
 import (
-	"net"
-	"strconv"
-	"strings"
 	"testing"
 	"time"
 )
 
 func TestListenTCP(t *testing.T) {
-	listeningPort, err := net.Listen("tcp", "127.0.0.1:0")
-	defer listeningPort.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	port := listeningPort.Addr()
-	effectivePort := strings.Split(port.String(), ":")[1]
-	err = WaitForPort("tcp", "127.0.0.1", strconv.Itoa(effectivePort), 5*time.Second)
+	err := WaitForPort("tcp", "127.0.0.1", RandomPort("tcp"), 5*time.Second)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestListenUDP(t *testing.T) {
-	listeningPort, err := net.Listen("tcp", "127.0.0.1:0")
-	defer listeningPort.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	port := listeningPort.Addr()
-	effectivePort := strings.Split(port.String(), ":")[1]
-	err = WaitForPort("udp", "127.0.0.1", strconv.Itoa(effectivePort), 5*time.Second)
+	err := WaitForPort("udp", "127.0.0.1", RandomPort("udp"), 5*time.Second)
 	if err != nil {
 		t.Fatal(err)
 	}
